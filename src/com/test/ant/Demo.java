7fc900ded1259c4f541cf5eaaea2cb9d43fc3c52
chandan ghosh <chandan493@gmail.com>
2016-06-25 20:59:46 +0530

    updated sysout message

--- src/com/test/ant/Demo.java
+++ src/com/test/ant/Demo.java
@@ -2,6 +2,7 @@ package com.test.ant;
 
 public class Demo {
 	public static void main(String[] args) {
+		System.out.println("This is a test application...");
 		System.out.println("Executing demo...");
 	}
 }
