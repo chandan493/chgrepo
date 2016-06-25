fcdad1834a1290cb3a46e88920def43c1c2ac473
chandan ghosh <chandan493@gmail.com>
2016-06-25 21:03:38 +0530

    updated demo.java

--- src/com/test/ant/Demo.java
+++ src/com/test/ant/Demo.java
@@ -2,7 +2,6 @@ package com.test.ant;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("This is a test application...");
 		System.out.println("Executing demo...");
 	}
 }
